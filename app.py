4b6432c78f02356b38234063ab9de3a2c8a92219
Maarten <1044232@hr.nl>
2023-02-03 14:27:35 +0100

    ADDED: Home + login directories

--- app.py
+++ app.py
@@ -9,9 +9,33 @@ FLASK_IP = LISTEN_ALL
 FLASK_PORT = 81
 FLASK_DEBUG = True
 
+@app.route("/")
+def index():
+    return render_template("globalhome.html")
+
+@app.route("/docentlogin")
+def docentlogin():
+    return render_template("docentlogin.html")
+
+@app.route("/studentlogin")
+def studentlogin():
+    return render_template("studentlogin.html")
+
+@app.route("/adminlogin")
+def adminlogin():
+    return render_template("adminlogin.html")
+
 @app.route("/docenthome")
 def docenthome():
     return render_template("docenthome.html")
+
+@app.route("/studenthome")
+def studenthome():
+    return render_template("studenthome.html")
+
+@app.route("/adminhome")
+def adminhome():
+    return render_template("adminhome.html")
 
 if __name__ == "__main__":
     app.run(host=FLASK_IP, port=FLASK_PORT, debug=FLASK_DEBUG)
