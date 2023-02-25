a3de9e5e14459e23739018382c34a3aa1d6bab7f
Shantelly-1052671 <113017279+Shantelly-1052671@users.noreply.github.com>
2023-02-25 21:20:28 +0100

    Navigatie toegevoegd samen met javascrpt. cancel button werkt om terug te gaan naar homepagina. css toegevoegd

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import render_template, url_for, flash, request, redirect, Response
 import sqlite3
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 
+
 app = Flask(__name__)
 app.debug=True
-app.secret_key = 'MachineKey'
+app.secret_key = '39201511d67c769063efb781831eac5cb95b460efbbab33a9ef9f469c13df166'
 
 # a simple demo dataset will be created.
 LISTEN_ALL = "0.0.0.0"
@@ -13,8 +14,30 @@ FLASK_IP = LISTEN_ALL
 FLASK_PORT = 81
 FLASK_DEBUG = True
 
-login_manager = LoginManager()
+login_manager = LoginManager(app)
 login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self, id, email, password):
+         self.id = (id)
+         self.email = email
+         self.password = password
+         self.authenticated = False
+    def is_active(self):
+         return self.is_active()
+    def is_anonymous(self):
+         return False
+    def is_authenticated(self):
+         return self.authenticated
+    def is_active(self):
+         return True
+    def get_id(self):
+         return self.id
+
+db = sqlite3
+@login_manager.user_loader
+def load_user(user_id):
+        return User(int(user_id))
 
 @app.route("/")
 def index():
@@ -26,11 +49,10 @@ def docentlogin():
 
 @app.route("/studentlogin")
 def studentlogin():
-    return render_template('studentlogin.html')
-
-@app.route("/adminlogin")
-def adminlogin():
-    return render_template("adminlogin.html")
+    return render_template("studentlogin.html")
+@app.route("/nav")
+def nav():
+    return render_template("nav.html")
 
 @app.route("/docenthome")
 def docenthome():
@@ -43,6 +65,11 @@ def studenthome():
 @app.route("/adminhome")
 def adminhome():
     return render_template("adminhome.html")
+
+@app.route("/adminlogin")
+def adminlogin():
+    return render_template("adminlogin.html")
+
 
 if __name__ == "__main__":
     app.run(host=FLASK_IP, port=FLASK_PORT, debug=FLASK_DEBUG)
